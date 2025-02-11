4f4e9c59e8af0312e3ebe2d57fb32ae022b6572b
jakebogan01 <jakebogan01@gmail.com>
2025-02-11 13:34:04 -0500

    adds vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,13 @@
 <script>
 	import '../app.css';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+	import { injectSpeedInsights } from '@vercel/speed-insights/sveltekit';
 	import { global_data } from '$lib/stores/globalData.svelte.js';
 	import Search from '$lib/components/Search.svelte';
 	import Header from '$lib/components/Header.svelte';
 	import Hero from '$lib/components/Hero.svelte';
+	injectAnalytics();
+	injectSpeedInsights();
 
 	let { children } = $props();
 </script>
